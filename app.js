81ab5a5b72f6a9d092c73880c32ec2486fdc2336
adnissen <andrew_nissen@yahoo.com>
2014-10-05 10:13:24 -0400

    allow anyone to connect

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://lage-server.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
