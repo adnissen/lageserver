b7ad02146364e332ec9ee57ca7d305abfe7974d0
adnissen <andrew_nissen@yahoo.com>
2014-10-05 10:10:07 -0400

    updated the header to be the right server

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://lage-server.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
