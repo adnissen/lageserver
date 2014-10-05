5afd8a1f5cedef1e4f4df59fcd288ee4f0789a8c
adnissen <andrew_nissen@yahoo.com>
2014-10-04 21:01:48 -0400

    the server will now actually listen

--- app.js
+++ app.js
@@ -33,3 +33,6 @@ app.get('/lookup', function(req, res){
 });
 
 app.set('port', (process.env.PORT || 5000))
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
